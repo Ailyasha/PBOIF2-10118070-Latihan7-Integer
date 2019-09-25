b4b8ec54edb18b97994a027cbf1dc7cebc4a5485
Aufa Ilyasha <55798997+Ailyasha@users.noreply.github.com>
2019-09-25 22:32:33 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118070/latihan7/integer/PBOIF210118070Latihan7Integer.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118070.latihan7.integer;
+
+/**
+ *
+ * @author
+ * NAMA     : Aufa Ilyasha
+ * Kelas    : IF2
+ * NIM      : 10118070
+ */
+public class PBOIF210118070Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 900000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int: " + i);
+        System.out.println("long : " + l);
+    }
+    
+}
